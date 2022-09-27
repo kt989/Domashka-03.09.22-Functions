baff092aecebd47ce2f408d9ea0db54879573e95
ДОМ <@DESKTOP-S2HEBGI>
2022-09-27 23:23:44 +0300

    Function overloading

--- Domashka 03.09.22 Functions/Source.cpp
+++ Domashka 03.09.22 Functions/Source.cpp
@@ -1,19 +1,78 @@
 #include <iostream>
 #include <conio.h>
+#include <iomanip>
 #include <stdlib.h>
 #include <time.h>
 using namespace std;
 
-void FillRand(int arr[], int size);
+#define DIF "\n-------------------------------------------------------------------------------\n"
+
+void FillRand(int arr[], int size, int MinRand=0, int MaxRand=100);
+void FillRand(double arr[], int size, int MinRand = 0, int MaxRand = 100);
+void FillRand(float arr[], int size, int MinRand = 0, int MaxRand = 100);
+void FillRand(char arr[], int size);
+
+void FillRand_1(int arr[], int size);//Заполнение массива повторяющимися числами
+
 void Print (int arr[], int size);
+void Print(double arr[], int size);
+void Print(float arr[], int size);
+void Print(char arr[], int size);
+
 void ReversePrint(int arr[], int size);
+void ReversePrint(double arr[], int size);
+void ReversePrint(float arr[], int size);
+void ReversePrint(char arr[], int size);
+
 int Sum(int arr[], int size);
-double Avg(int sum, int size);
+double Sum(double arr[], int size);
+float Sum(float arr[], int size);
+char Sum(char arr[], int size);
+
+
+double Avg(int arr[], int size);
+double Avg(double arr[], int size);
+double Avg(float arr[], int size);
+char Avg(char arr[], int size);
+
 int minValueIn(int arr[], int size);
+double minValueIn(double arr[], int size);
+float minValueIn(float arr[], int size);
+char minValueIn(char arr[], int size);
+
 int maxValueIn(int arr[], int size);
+double maxValueIn(double arr[], int size);
+float maxValueIn(float arr[], int size);
+char maxValueIn(char arr[], int size);
+
+
 void shiftLeft(int arr[], int size, int step);
+void shiftLeft(double arr[], int size, int step);
+void shiftLeft(float arr[], int size, int step);
+void shiftLeft(char arr[], int size, int step);
+
 void shiftRight(int arr[], int size, int step);
+void shiftRight(double arr[], int size, int step);
+void shiftRight(float arr[], int size, int step);
+void shiftRight(char arr[], int size, int step);
+
+
+void Sort_up(int arr[], int size); //Олег, еще добавила сортировку массива
+void Sort_up(double arr[], int size);
+void Sort_up(float arr[], int size);
+void Sort_up(char arr[], int size);
+
+void Sort_down(int arr[], int size);
+void Sort_down(double arr[], int size);
+void Sort_down(float arr[], int size);
+void Sort_down(char arr[], int size);
+
+void Search(int arr[], int size);// и поиск повторяющихся чисел 
 void UniqueRand(int arr[], int size);
+void UniqueRand(double arr[], int size);
+void UniqueRand(float arr[], int size);
+void UniqueRand(char arr[], int size);
+
 
 void main()
 {
@@ -21,63 +80,193 @@ void main()
 	cout << endl << "Дано: массив из 10 элементов "; 
 	cout << endl << endl << "Для заполнения массива случайными числами нажмите Enter ";
 	int key = _getch();
-	
+	//cout << key;
 	while (key != 13) key = _getch();
 	const int size = 10;
 	int arr[size] = {};
+	double brr[size] = {};
+	float crr[size] = {};
+	char drr[size] = {};
+
 	srand(time(NULL));
 
-	FillRand(arr, size);
-	Print(arr, size);
+    FillRand(arr, size);
+	FillRand(brr, size);
+	FillRand(crr, size);
+	FillRand(drr, size);
 
-	cout << endl << endl << "Для вывода массива в обратном порядке нажмите Enter ";
+	Print(arr, size);
+	Print(brr, size);
+	Print(crr, size);
+	Print(drr, size);
+	cout << DIF;
+
+	cout << endl << endl << "Для вывода массивов в обратном порядке нажмите Enter ";
 	key = _getch();
 	while (key != 13) key = _getch();
 	ReversePrint(arr, size);
+	ReversePrint(brr, size);
+	ReversePrint(crr, size);
+	ReversePrint(drr, size);
+	cout << DIF<<endl<<endl;
 
-	cout << endl << endl << "Для вывода суммы элементов массива нажмите Enter    ";
+	cout << endl << endl << "Для вывода суммы элементов массивов нажмите Enter    ";
 	key = _getch();
 	while (key != 13) key = _getch();
-	int sum = Sum(arr, size);
-	cout<<sum<<endl<< endl;
+	cout<< endl << endl <<Sum(arr, size) <<endl<< endl;
+	cout << Sum(brr, size) << endl << endl;
+	cout << Sum(crr, size) << endl << endl;
+	cout << Sum(drr, size) << endl << DIF << endl;
 
-	cout << endl << endl << "Для вывода среднего значения элементов массива нажмите Enter    ";
+	cout << endl << endl << "Для вывода среднего значения элементов массивов нажмите Enter    ";
 	key = _getch();
 	while (key != 13) key = _getch();
-	cout << Avg(sum, size) << endl << endl;
+	cout << endl << endl << Avg(arr, size) << endl << endl;
+	cout << Avg(brr, size) << endl << endl;
+	cout << Avg(crr, size) << endl << endl;
+	cout << Avg(drr, size) << endl << DIF << endl;
 
-	cout << endl << endl << "Для вывода минимального значения элементов массива нажмите Enter    ";
+	cout << endl << endl << "Для вывода минимального значения элементов массивов нажмите Enter    ";
 	key = _getch();
 	while (key != 13) key = _getch();
-	cout << minValueIn(arr, size) << endl << endl;
+	cout << endl<< endl<< minValueIn(arr, size) << endl << endl;
+	cout << minValueIn(brr, size) << endl << endl;
+	cout << minValueIn(crr, size) << endl << endl;
+	cout << minValueIn(drr, size) << endl << DIF << endl;
 
-	cout << endl << endl << "Для вывода максимального значения элементов массива нажмите Enter    ";
+	cout << endl << endl << "Для вывода максимального значения элементов массивов нажмите Enter    ";
 	key = _getch();
 	while (key != 13) key = _getch();
-	cout << maxValueIn(arr, size) << endl << endl;
+	cout << endl << endl << maxValueIn(arr, size) << endl << endl;
+	cout << maxValueIn(brr, size) << endl << endl;
+	cout << maxValueIn(crr, size) << endl << endl;
+	cout << maxValueIn(drr, size) << endl << DIF << endl;
+
+	cout << endl << endl << "Для продолжения нажмите Enter    ";
+	key = _getch();
+	while (key != 13) key = _getch();
+
+	system("CLS");
+	Print(arr, size);
+	Print(brr, size);
+	Print(crr, size);
+	Print(drr, size);
+	cout << DIF << endl << endl;
 
 	cout << endl << endl << "Введите число, показывающее на какое количество позиций необходимо сдвинуть массив влево:   ";
 	int step;
 	cin >> step;
 	shiftLeft(arr, size, step);
+	shiftLeft(brr, size, step);
+	shiftLeft(crr, size, step);
+	shiftLeft(drr, size, step);
+	cout << DIF << endl<<endl;
 
 	cout << endl << endl << "Введите число, показывающее на какое количество позиций необходимо сдвинуть массив вправо:   ";
 	cin >> step;
 	shiftRight(arr, size, step);
+	shiftRight(brr, size, step);
+	shiftRight(crr, size, step);
+	shiftRight(drr, size, step);
+	cout << DIF << endl << endl;
 	
+	cout << endl << endl << "Для сортировки по ээлементов массивов по возрастанию нажмите 1";
+	cout << endl << endl << "Для сортировки по ээлементов массивов по убыванию нажмите 0";
+	do
+	{
+		key = _getch();
+		if (key == 48) // 0
+		{ 
+			Sort_down(arr, size);
+			Sort_down(brr, size);
+			Sort_down(crr, size);
+			Sort_down(drr, size);
+			cout << endl << "Массивы по убыванию " << endl << endl;
+			Print(arr, size);
+			Print(brr, size);
+			Print(crr, size);
+			Print(drr, size);
+			cout << endl << DIF << endl << endl;
+			break;
+		}
+			
+		else if (key == 49) //1
+		{
+			Sort_up(arr, size);
+			Sort_up(brr, size);
+			Sort_up(crr, size);
+			Sort_up(drr, size);
+			cout << endl << "Массивы по возрастанию " << endl << endl;
+			Print(arr, size);
+			Print(brr, size);
+			Print(crr, size);
+			Print(drr, size);
+			cout << endl << DIF << endl << endl;
+			break;
+		}
+		else cout << "Повторите ввод";
+	} while (key != 48 || key != 49);
+
 	cout << endl << endl << "Для замены повторяющихся ээлементов массива нажмите Enter    ";
 	key = _getch();
 	while (key != 13) key = _getch();
 	UniqueRand(arr, size);
+	UniqueRand(brr, size);
+	UniqueRand(crr, size);
+	UniqueRand(drr, size);
+	cout << endl << DIF << endl << endl;
+
+	cout << endl << endl << "Для продолжения нажмите Enter    ";
+	key = _getch();
+	while (key != 13) key = _getch();
+
+	system("CLS");
+	cout << "  Для вывода нового массива нажмите Enter" << endl << endl;
+	key = _getch();
+	while (key != 13) key = _getch();
+	FillRand_1(arr, size);
+	Print(arr, size);
+	cout << endl << endl;
+	Search(arr, size);
+
 
 }
 
-void FillRand(int arr[], int size)
+void FillRand(int arr[], int size, int MinRand, int MaxRand)
 {
-	for (int i = 0;i < 10;i++)
-		arr[i] = rand()%100;
+	for (int i = 0;i < size;i++)
+		arr[i] = rand() % (MaxRand-MinRand) + MinRand;
 }
 
+void FillRand(double arr[], int size, int MinRand, int MaxRand)
+{
+	for (int i = 0;i < size;i++)
+	{
+		arr[i] = (rand() % (MaxRand - MinRand) + MinRand);
+	    arr[i] /= 100;
+    }
+}
+
+void FillRand(float arr[], int size, int MinRand, int MaxRand)
+{
+	for (int i = 0;i < size;i++)
+	{
+		arr[i] = (rand() % (MaxRand - MinRand) + MinRand);
+		arr[i] /= 100;
+	}
+}
+
+void FillRand(char arr[], int size)
+{
+	for (int i = 0;i < size;i++)
+		arr[i] = char(rand() % 255) ;
+}
+
+void FillRand_1(int arr[], int size)
+{
+	for (int i = 0;i < size;i++)
+		arr[i] = rand() % size/2;
+}
 void Print(int arr[], int size)
 {
 	cout << endl << endl;
@@ -87,12 +276,65 @@ void Print(int arr[], int size)
 	}
 }
 
+void Print(double arr[], int size)
+{
+	cout << endl << endl;
+	for (int i = 0;i < size; i++)
+	{
+		cout << setprecision (2)<<arr[i] << "\t";
+	}
+}
+
+void Print(float arr[], int size)
+{
+	cout << endl << endl;
+	for (int i = 0;i < size; i++)
+	{
+		cout << setprecision(3) << arr[i] << "\t";
+	}
+}
+void Print(char arr[], int size)
+{
+	cout << endl << endl;
+	for (int i = 0;i < size; i++)
+	{
+		cout << char (arr[i]) << "\t";
+	}
+}
+
 void ReversePrint(int arr[], int size)
 {
 	cout << endl << endl;
 	for (int i = size-1;i >= 0; i--)
 	{
 		cout << arr[i] << "\t";
+	}
+}
+
+void ReversePrint(double arr[], int size)
+{
+	cout << endl << endl;
+	for (int i = size - 1;i >= 0; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+}
+
+void ReversePrint(float arr[], int size)
+{
+	cout << endl << endl;
+	for (int i = size - 1;i >= 0; i--)
+	{
+		cout << arr[i] << "\t";
+	}
+}
+
+void ReversePrint(char arr[], int size)
+{
+	cout << endl << endl;
+	for (int i = size - 1;i >= 0; i--)
+	{
+		cout << char(arr[i]) << "\t";
 	}
 }
 
@@ -106,10 +348,55 @@ int Sum(int arr[], int size)
 	return (sum);
 }
 
-double Avg(int sum, int size)
+double Sum(double arr[], int size)
 {
-	double avg = double(sum) / double(size);
-	return (avg);
+	double sum = 0;
+	for (int i = 0;i < 10; i++)
+	{
+		sum += arr[i];
+	}
+	return (sum);
+}
+
+float Sum(float arr[], int size)
+{
+	float sum = 0;
+	for (int i = 0;i < 10; i++)
+	{
+		sum += arr[i];
+	}
+	return (sum);
+}
+
+char Sum(char arr[], int size)
+{
+	int sum = 0;
+	for (int i = 0;i < 10; i++)
+	{
+		sum += arr[i];
+	}
+	sum = sum % 255;
+	return (char(sum));
+}
+
+double Avg(int arr[], int size)
+{
+   return Sum(arr,size)/double(size);
+}
+
+double Avg(double arr[], int size)
+{
+    return Sum(arr,size)/size;
+}
+
+double Avg (float arr[], int size)
+{
+	return Sum(arr, size) / size;
+}
+
+char Avg(char arr[], int size)
+{
+	return char(Sum(arr, size) / size);
 }
 
 int minValueIn(int arr[], int size)
@@ -122,6 +409,35 @@ int minValueIn(int arr[], int size)
 	return (minValue);
 }
 
+double minValueIn(double arr[], int size)
+{
+	double minValue = arr[0];
+	for (int i = 0;i < size;i++)
+	{
+		if (minValue > arr[i]) minValue = arr[i];
+	}
+	return (minValue);
+}
+
+float minValueIn(float arr[], int size)
+{
+	float minValue = arr[0];
+	for (int i = 0;i < size;i++)
+	{
+		if (minValue > arr[i]) minValue = arr[i];
+	}
+	return (minValue);
+}
+
+char minValueIn(char arr[], int size)
+{
+	char minValue = arr[0];
+	for (int i = 0;i < size;i++)
+	{
+		if (minValue > arr[i]) minValue = arr[i];
+	}
+	return (char (minValue));
+}
 
 int maxValueIn(int arr[], int size)
 {
@@ -133,6 +449,35 @@ int maxValueIn(int arr[], int size)
 	return (maxValue);
 }
 
+double maxValueIn(double arr[], int size)
+{
+	double maxValue = arr[0];
+	for (int i = 0;i < size;i++)
+	{
+		if (maxValue < arr[i]) maxValue = arr[i];
+	}
+	return (maxValue);
+}
+
+float maxValueIn(float arr[], int size)
+{
+	float maxValue = arr[0];
+	for (int i = 0;i < size;i++)
+	{
+		if (maxValue < arr[i]) maxValue = arr[i];
+	}
+	return (maxValue);
+}
+
+char maxValueIn(char arr[], int size)
+{
+	char maxValue = arr[0];
+	for (int i = 0;i < size;i++)
+	{
+		if (maxValue < arr[i]) maxValue = arr[i];
+	}
+	return (char(maxValue));
+}
 void shiftRight(int arr[], int size, int step)
 {
 	int sdvig = 0;
@@ -159,6 +504,95 @@ void shiftRight(int arr[], int size, int step)
 			arr[j] = arr[j + 1];
 		}
 		arr[size-1] = sdvig;
+	}
+
+}
+
+void shiftRight(double arr[], int size, int step)
+{
+	double sdvig = 0;
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[size - 1];
+		for (int j = size - 1; j > 0; j--)
+		{
+			arr[j] = arr[j - 1];
+		}
+		arr[0] = sdvig;
+	}
+	cout << endl << endl;
+	for (int i = 0;i < size;i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[0];
+		for (int j = 0; j < size - 1; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[size - 1] = sdvig;
+	}
+
+}
+
+void shiftRight(float arr[], int size, int step)
+{
+	float sdvig = 0;
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[size - 1];
+		for (int j = size - 1; j > 0; j--)
+		{
+
+			arr[j] = arr[j - 1];
+		}
+		arr[0] = sdvig;
+	}
+	cout << endl << endl;
+	for (int i = 0;i < size;i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[0];
+		for (int j = 0; j < size - 1; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[size - 1] = sdvig;
+	}
+
+}
+
+void shiftRight(char arr[], int size, int step)
+{
+	char sdvig = 0;
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[size - 1];
+		for (int j = size - 1; j > 0; j--)
+		{
+
+			arr[j] = arr[j - 1];
+		}
+		arr[0] = sdvig;
+	}
+	cout << endl << endl;
+	for (int i = 0;i < size;i++)
+	{
+		cout << char(arr[i]) << "\t";
+	}
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[0];
+		for (int j = 0; j < size - 1; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[size - 1] = sdvig;
 	}
 
 }
@@ -193,6 +627,96 @@ void shiftLeft(int arr[], int size, int step)
 
 }
 
+void shiftLeft(double arr[], int size, int step)
+{
+	double sdvig = 0;
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[0];
+		for (int j = 0; j < size - 1; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[size - 1] = sdvig;
+	}
+	cout << endl << endl;
+	for (int i = 0;i < size;i++)
+	{
+		cout << arr[i] << "\t";
+	}
+
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[size - 1];
+		for (int j = size - 1; j > 0; j--)
+		{
+			arr[j] = arr[j - 1];
+		}
+		arr[0] = sdvig;
+	}
+
+}
+
+void shiftLeft(float arr[], int size, int step)
+{
+	float sdvig = 0;
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[0];
+		for (int j = 0; j < size - 1; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[size - 1] = sdvig;
+	}
+	cout << endl << endl;
+	for (int i = 0;i < size;i++)
+	{
+		cout << arr[i] << "\t";
+	}
+
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[size - 1];
+		for (int j = size - 1; j > 0; j--)
+		{
+			arr[j] = arr[j - 1];
+		}
+		arr[0] = sdvig;
+	}
+
+}
+
+void shiftLeft(char arr[], int size, int step)
+{
+	char sdvig = 0;
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[0];
+		for (int j = 0; j < size - 1; j++)
+		{
+			arr[j] = arr[j + 1];
+		}
+		arr[size - 1] = sdvig;
+	}
+	cout << endl << endl;
+	for (int i = 0;i < size;i++)
+	{
+		cout << char(arr[i]) << "\t";
+	}
+
+	for (int i = 0;i < step;i++)
+	{
+		sdvig = arr[size - 1];
+		for (int j = size - 1; j > 0; j--)
+		{
+			arr[j] = arr[j - 1];
+		}
+		arr[0] = sdvig;
+	}
+
+}
+
 void UniqueRand(int arr[], int size)
 {
 	for (int i = 0;i < size;i++)
@@ -207,6 +731,228 @@ void UniqueRand(int arr[], int size)
 	cout << endl;
 	for (int i = 0;i < size;i++) cout << arr[i] << "\t";
 	cout << endl << endl;
+}
+
+void UniqueRand(double arr[], int size)
+{
+	for (int i = 0;i < size;i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (i == j) continue;
+			else if (arr[i] == arr[j])  arr[j] = rand() % 100;
+		}
+	}
+
+	cout << endl;
+	for (int i = 0;i < size;i++) cout << arr[i] << "\t";
+	cout << endl << endl;
+}
 
 
+void UniqueRand(float arr[], int size)
+{
+	for (int i = 0;i < size;i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (i == j) continue;
+			else if (arr[i] == arr[j])  arr[j] = rand() % 100;
+		}
+	}
+
+	cout << endl;
+	for (int i = 0;i < size;i++) cout << arr[i] << "\t";
+	cout << endl << endl;
+}
+
+
+void UniqueRand(char arr[], int size)
+{
+	for (int i = 0;i < size;i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			if (i == j) continue;
+			else if (arr[i] == arr[j]) arr[j] = char(rand() % 255);//что-то не работает для char
+		}
+	}
+
+	cout << endl;
+	for (int i = 0;i < size;i++) cout << char(arr[i]) << "\t";
+	cout << endl << endl;
+}
+
+void Sort_up(int arr[], int size)
+{
+	int buffer;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i+1; j < size; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				buffer = arr[j];
+				arr[j] = arr[i];
+				arr[i] = buffer;
+			}
+		}
+	}
+
+}
+
+void Sort_up(double arr[], int size)
+{
+	double buffer;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				buffer = arr[j];
+				arr[j] = arr[i];
+				arr[i] = buffer;
+			}
+		}
+	}
+
+}
+
+void Sort_up(float arr[], int size)
+{
+	float buffer;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				buffer = arr[j];
+				arr[j] = arr[i];
+				arr[i] = buffer;
+			}
+		}
+	}
+
+}
+
+void Sort_up(char arr[], int size)
+{
+	char buffer;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				buffer = arr[j];
+				arr[j] = arr[i];
+				arr[i] = buffer;
+			}
+		}
+	}
+
+}
+
+void Sort_down(int arr[], int size)
+{
+	int buffer;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[i] < arr[j])
+			{
+				buffer = arr[j];
+				arr[j] = arr[i];
+				arr[i] = buffer;
+			}
+		}
+	}
+
+}
+
+void Sort_down(double arr[], int size)
+{
+	double buffer;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[i] < arr[j])
+			{
+				buffer = arr[j];
+				arr[j] = arr[i];
+				arr[i] = buffer;
+			}
+		}
+	}
+
+}
+
+void Sort_down(float arr[], int size)
+{
+	float buffer;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[i] < arr[j])
+			{
+				buffer = arr[j];
+				arr[j] = arr[i];
+				arr[i] = buffer;
+			}
+		}
+	}
+
+}
+
+void Sort_down(char arr[], int size)
+{
+	char buffer;
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[i] < arr[j])
+			{
+				buffer = arr[j];
+				arr[j] = arr[i];
+				arr[i] = buffer;
+			}
+		}
+	}
+
+}
+
+void Search(int arr[], int size)
+{
+	int count;
+	cout << endl << "Значение\t\tКоличество повторений" << endl << endl;
+	for (int i = 0;i < size;i++)
+	{
+		count = 1;
+		for (int j=0; j < size;j++)
+		{
+			if (i == j) continue;
+			if (arr[i] == arr[j])
+			{
+				count++;
+			}
+		}
+		for (int j = 0;j <= i;j++)
+		{
+			if (i == j) continue;
+			if (arr[i] != arr[j]) continue;
+			else if (count > 1)
+			{
+				cout << arr[i] << "\t\t\t" << count << endl << endl;
+				
+			}
+		}
+
+
+	}
 }
